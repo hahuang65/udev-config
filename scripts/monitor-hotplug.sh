da150cabc2aa8c3b6f0a84c204a94a62bb20d888
Howard Huang <hao@hwrd.me>
2018-03-08 16:34:31 -0800

    Fix script calling the wrong directory

--- scripts/monitor-hotplug.sh
+++ scripts/monitor-hotplug.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/home/hao/.dotfiles/scripts/select_video_output && /home/hao/.dotfiles/scripts/select_audio_output
+/home/hao/.dotfiles/udev/scripts/select_video_output && /home/hao/.dotfiles/udev/scripts/select_audio_output
