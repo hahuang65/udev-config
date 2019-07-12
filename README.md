b4920317ed4e31325ae4c0b2c44236c5cb1c5f52
Howard Huang <hao@hwrd.me>
2019-07-12 11:10:19 -0700

    Enable/disable BT based on docked or not

--- README.md
+++ README.md
@@ -8,5 +8,5 @@ sudo udevadm test /sys/class/drm/card0
 
 To setup autorandr, use xrandr to set up the display, and save it with autorandr:
 ```sh
-xrandr --output "eDP1" --off --output "DP1" --mode 2160x1440
+xrandr --output "eDP-1" --off --output "DP-1" --mode 3840x2160
 ```
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $(uname) = 'Linux' ]; then
-  # sudo ln -sf "${PWD}/rules/hdmi-bt.rules" "/etc/udev/rules.d/90-hdmi-bt.rules"
+  sudo ln -sf "${PWD}/rules/hdmi-bt.rules" "/etc/udev/rules.d/90-hdmi-bt.rules"
   sudo ln -sf "${PWD}/rules/hdmi-video.rules" "/etc/udev/rules.d/95-hdmi-video.rules"
   sudo ln -sf "${PWD}/rules/hdmi-audio.rules" "/etc/udev/rules.d/99-hdmi-audio.rules"
 
