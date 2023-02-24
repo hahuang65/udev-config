dcc68f5fd6875f52da92dda5327b25551737a56c
Howard Huang <hao@hwrd.me>
2023-02-23 21:37:15 -0800

    Remove un-used rules, add Bluetooth power management rules

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
 if [ $(uname) = 'Linux' ]; then
-  sudo ln -sf "${PWD}/rules/hdmi-video.rules" "/etc/udev/rules.d/95-hdmi-video.rules"
-  sudo ln -sf "${PWD}/rules/hdmi-audio.rules" "/etc/udev/rules.d/99-hdmi-audio.rules"
-
-  for file in ${PWD}/scripts/*; do
-    sudo ln -sf $file /usr/local/bin
-  done
+	sudo ln -sf "${PWD}/rules/bluetooth-power-management.rules" "/etc/udev/rules.d/50-bluetooth-power-management.rules"
 fi
